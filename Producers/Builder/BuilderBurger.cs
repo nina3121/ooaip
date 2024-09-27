0778bf929b9e9b9944c314c7c1f64e1a64cad012
nina3121 <144561678+nina3121@users.noreply.github.com>
2024-09-27 21:45:57 +0300

    12

--- Producers/Builder/BuilderBurger.cs
+++ Producers/Builder/BuilderBurger.cs
@@ -5,16 +5,5 @@
         protected Burger _burger;
         public virtual void Reset()
         {
-            _burger = new Burger();
-        }
-        public virtual void WithCheese() { }
-        public virtual void WithMeat() { }
-        public virtual void WithTomato() { }
-        public virtual void WithSauce() { }
-        public virtual void WithSalad() { }
-        public Burger GetBurger()
-        {
-            return _burger;
-        }
     }
 }
