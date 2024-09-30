15c1baf09f86297f9c4cd7dc0855a5dc62ecbb67
nina3121 <144561678+nina3121@users.noreply.github.com>
2024-09-30 11:18:20 +0300

    12

---
+++ Producers/Singleton/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Client
+    {
+        private IChair _chair;
+        private ISofa _sofa;
+        private IWardrobe _wardrobe;
+        public Client(IAbstractFactory factory)
+        {
+            _chair = factory.CreateChair();
+            _sofa = factory.CreateSofa();
+            _wardrobe = factory.CreateWardrobe();
+        }
+        public void Get()
+        {
+            _chair.GetChair();
+            _sofa.GetSofa();
+            _wardrobe.GetWardrobe();
+        }
+    }
+}
