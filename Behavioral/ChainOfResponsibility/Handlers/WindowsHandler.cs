f0639b9b776a41342badc4c5f62cadeba4dd2db6
nina3121 <144561678+nina3121@users.noreply.github.com>
2024-12-23 06:41:39 +0200

    12

--- Behavioral/ChainOfResponsibility/Handlers/WindowsHandler.cs
+++ Behavioral/ChainOfResponsibility/Handlers/WindowsHandler.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ChainOfResponsibility
+namespace ChainOfResponsibility.Handlers
 {
     public class WindowsHandler : IHandler
     {
